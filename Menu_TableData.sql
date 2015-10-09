758222d967c0e619f82b43448c898fa3baa0d9ce
MonsieurPotato <mr_kken_91th@yahoo.co.jp>
2015-10-10 00:49:12 +0900

    They insert records to tables.

---
+++ Menu_TableData.sql
@@ -0,0 +1,11 @@
+/* Category_List Data*/
+INSERT  INTO  Menu.Category_List  VALUES  ( '0' , '生鮮' ) ;
+INSERT  INTO  Menu.Category_List  VALUES  ( '1' , '焼く' ) ;
+INSERT  INTO  Menu.Category_List  VALUES  ( '2' , '煮る' ) ;
+INSERT  INTO  Menu.Category_List  VALUES  ( '3' , '揚げる' ) ;
+INSERT  INTO  Menu.Category_List  VALUES  ( '4' , '蒸す' ) ;
+/* Ganre_List Data*/
+INSERT  INTO  Menu.Ganre_List VALUES  ( '0' , 'その他' ) ;
+INSERT  INTO  Menu.Ganre_List VALUES  ( '1' , '和食'  ) ;
+INSERT  INTO  Menu.Ganre_List VALUES  ( '2' , '洋食'  ) ;
+INSERT  INTO  Menu.Ganre_List VALUES  ( '3' , '中華'  ) ;
